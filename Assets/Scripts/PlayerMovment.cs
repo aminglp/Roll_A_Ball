4b0d60db8f44d6d8fad524bb684e85ccb0cb4aa0
Mohamd Amin Golparast <amin.glp177@gmail.com>
2025-07-18 22:07:59 +0330

    move(ToDo)

--- Assets/Scripts/PlayerMovment.cs
+++ Assets/Scripts/PlayerMovment.cs
@@ -2,15 +2,26 @@ using UnityEngine;
 
 public class PlayerMovment : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private float _moveSpeed;
+    private Rigidbody _rigidbody;
+
     void Start()
     {
-        
+        _rigidbody = GetComponent<Rigidbody>();
+        // float horizontal = Input.GetAxis("Horizontal");
+        // float vertical = Input.GetAxis("Vertical");
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        move();
+    }
+
+    private void move()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 dirction = new Vector3(horizontal,0 ,vertical );
+        _rigidbody.angularVelocity = dirction*_moveSpeed;
     }
 }
